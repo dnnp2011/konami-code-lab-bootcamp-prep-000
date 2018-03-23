ca86108ad239ffdbe6cbfb046fa49ec20a94bbde
Dalton Pierce <dnnp2011@gmail.com>
2018-03-23 05:25:49 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,13 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+var keypresses = [];
+
 function init() {
-  
+  //Add event listeners
+}
+
+function detectKeys(e){
+  //Call on event
+  //Add keys to Array
+  //If array is full, check if presses match konamiCode
 }
